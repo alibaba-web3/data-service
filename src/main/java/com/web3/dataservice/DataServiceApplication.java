312fcc8c272f5204c12d10fe012aefc8c716885f
mianyun.yt <mianyun.yt@alibaba-inc.com>
2022-12-23 14:19:44 +0800

    chore: init Spring

---
+++ src/main/java/com/web3/dataservice/DataServiceApplication.java
@@ -0,0 +1,13 @@
+package com.web3.dataservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DataServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DataServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/web3/dataservice/DataServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.web3.dataservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DataServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
