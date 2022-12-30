ae667963c587951689faae934055805bb875bf80
mianyun.yt <mianyun.yt@alibaba-inc.com>
2022-12-30 16:01:34 +0800

    feat: multi module package

--- start/src/main/java/com/web3/Application.java
+++ start/src/main/java/com/web3/Application.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DataServiceApplication {
+public class Application {
     public static void main(String[] args) {
-        SpringApplication.run(DataServiceApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 }
