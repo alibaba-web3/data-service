d0beab003d2df6724d9f292aaeaeafbdcc6504c5
mianyun.yt <mianyun.yt@alibaba-inc.com>
2022-12-30 16:09:43 +0800

    feat: scanBasePackages

--- start/src/main/java/com/web3/Application.java
+++ start/src/main/java/com/web3/Application.java
@@ -3,7 +3,7 @@ package com.web3;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"com.web3"})
 public class Application {
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
