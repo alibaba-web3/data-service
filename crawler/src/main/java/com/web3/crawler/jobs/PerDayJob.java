2594c4215d8ca4578de82a990b1ef91457806063
mianyun.yt <mianyun.yt@alibaba-inc.com>
2023-02-09 11:20:45 +0800

    feat: syncAllProtocol every day

---
+++ crawler/src/main/java/com/web3/crawler/jobs/PerDayJob.java
@@ -0,0 +1,26 @@
+package com.web3.crawler.jobs;
+
+import com.web3.service.tvl.TvlService;
+import jakarta.annotation.Resource;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: mianyun.yt
+ * @Date: 2023/2/9
+ */
+@Service
+public class PerDayJob {
+
+    @Resource
+    private TvlService tvlService;
+
+    /**
+     * 每天凌晨 2 点执行
+     */
+    @Scheduled(cron = "0 0 2 * * ?")
+    public void execute() {
+        tvlService.syncAllProtocol();
+    }
+
+}
