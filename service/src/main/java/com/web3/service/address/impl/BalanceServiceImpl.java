fa96e4205b336b2b0d10b27a0eaf5f19ee5fc549
mianyun.yt <mianyun.yt@alibaba-inc.com>
2023-03-04 20:38:22 +0800

    feat: core pool 1

--- service/src/main/java/com/web3/service/address/impl/BalanceServiceImpl.java
+++ service/src/main/java/com/web3/service/address/impl/BalanceServiceImpl.java
@@ -93,7 +93,7 @@ public class BalanceServiceImpl implements BalanceService {
         processBalanceExecutor = new ThreadPoolExecutor(800, 800, 0, TimeUnit.SECONDS,
             new LinkedBlockingQueue<>(), new ThreadFactoryBuilder().setNameFormat("get-balance-%d").build());
 
-        processAddressExecutor = new ThreadPoolExecutor(5, 5, 10, TimeUnit.SECONDS,
+        processAddressExecutor = new ThreadPoolExecutor(1, 1, 10, TimeUnit.SECONDS,
             new LinkedBlockingQueue<>(), new ThreadFactoryBuilder().setNameFormat("get-address-%d").build());
     }
 
