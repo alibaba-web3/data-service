f44444890d5f3a3824603ba0029761b3d698302e
fuxian <liuwang.liu@alibaba-inc.com>
2023-03-13 16:37:29 +0800

    feat: cors config

---
+++ start/src/main/java/com/web3/config/CorsConfig.java
@@ -0,0 +1,27 @@
+package com.web3.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+/**
+ * @Author fuxian
+ * @Date 2023/3/13
+ */
+@Configuration
+public class CorsConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry // 所有接口都支持跨域
+                .addMapping("/**")
+                // 允许访问的地址
+                .allowedOriginPatterns("https://alibaba-web3.vercel.app/")
+                .allowCredentials(true)
+                // 允许访问的 Rest 方法 "GET", "HEAD", "POST", "PUT", "DELETE", "OPTIONS" ...
+                .allowedMethods("*")
+                // 跨域允许时间
+                .maxAge(3600);
+    }
+
+}
